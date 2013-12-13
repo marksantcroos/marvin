f2deee08702b237b4a0492bb1d4bc3ac3027c789
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2013-12-13 02:18:34 +0100

    Expose the right objects.

--- marvin/__init__.py
+++ marvin/__init__.py
@@ -1,1 +1,7 @@
 __author__ = 'mark'
+
+from workflow_xml import xml2abstract
+from inputdata_xml import InputDataXML
+from abstractwf import AbstractWF
+from concretewf import ConcreteWF
+from iteration import print_tree
