694c56a223184037ee6f85b424376bbecb2ff6af
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2016-06-27 10:22:26 +0200

    Is the init file actually used?

--- marvin/__init__.py
+++ marvin/__init__.py
@@ -4,4 +4,5 @@ from workflow_xml import xml2abstract
 from inputdata_xml import InputDataXML
 from abstractwf import AbstractWF
 from concretewf import ConcreteWF
-from iteration import print_tree
+#from iteration import print_tree
+#from gasw import gasw_repo
