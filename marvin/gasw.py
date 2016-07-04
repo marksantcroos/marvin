c28269a84d41735d2c38aefed2b45a1f37e88ffd
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2016-07-04 11:59:47 +0200

    Add functions to gasw repo.

--- marvin/gasw.py
+++ marvin/gasw.py
@@ -1,27 +1,43 @@
+def s2f(task_no):
+    return ['fastq%d' % task_no]
+
+def split(task_no):
+    return [['fastq_slice_A'], ['fastq_slice_B']]
+
+def bwa(task_no):
+    return ['bam_%d' % task_no]
+
+def merge(task_no):
+    return ['bam_merged_%d' % task_no]
+
+
 gasw_repo = {
 
     "s2f.gasw": {
         #"executable": "solid-to-fastqPE"
         "executable": "sleep",
         "arguments": ['1'],
-
+        "function": s2f
     },
 
     "split.gasw": {
         # "executable": "split-fastq"
         "executable": "sleep",
-        "arguments": ['2'],
+        "arguments": ['1'],
+        "function": split
     },
 
     "bwa.gasw": {
         # "executable": "bwa-short-paired-read",
         "executable": "sleep",
-        "arguments": ['4'],
+        "arguments": ['1'],
+        "function": bwa
     },
 
     "merge.gasw": {
         # "executable": "merge-bam"
         "executable": "sleep",
-        "arguments": ['3'],
+        "arguments": ['1'],
+        "function": merge
     }
 }
