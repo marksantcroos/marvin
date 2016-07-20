b698316d83c766d9c501cf7ca698a9e691131769
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2016-07-21 01:32:00 +0200

    Use split and derive chunks.

--- marvin/gasw.py
+++ marvin/gasw.py
@@ -1,16 +1,18 @@
 import copy
 import string
 import radical.pilot as rp
+import math
 
 
+###########################################################################
+#
 def s2f(task_no, input):
     return ['fastq%d_%s' % (task_no, input)]
 
 
+###########################################################################
+#
 def split(self):
-    # chunks = 2
-    # return [['%s_slice_%s' % (self.input, letter)] for letter in string.ascii_uppercase[:chunks]]
-
     orig = self.output[0]
     self.output = []
     print 'files: %s' % orig.description.file_urls
@@ -26,27 +28,32 @@ def split(self):
         self.output.append(du)
 
 
+###########################################################################
+#
 def bwa(task_no, input):
     return ['%s.bam' % input[0]]
 
 
+###########################################################################
+#
 def merge(task_no, input):
     return ['%s_merged.bam' % input[0][0][:6]]
 
+chunks = 4
 
 gasw_repo = {
 
     "s2f.gasw": {
         "executable": "tr",
-        "arguments": ['[:upper:]', '[:lower:]', '<', '${INPUT}', '>', 'fastq.txt'],
-        "output": ['fastq.txt'],
+        "arguments": ['[:upper:]', '[:lower:]', '<', '${INPUT}', '>', '${INPUT}_fastq.txt'],
+        "output": ['${INPUT}_fastq.txt'],
         # "post_function": s2f
     },
 
     "split.gasw": {
-        "executable": "awk",
-        "arguments": ['NR%13==1 { file = "fastq_" sprintf("%04d", NR/13) } { print > file }', 'fastq.txt'],
-        "output": ['fastq_0000', 'fastq_0001'],
+        "executable": "split",
+        "arguments": ['-a', '1', '-l', str(int(math.ceil(26/float(chunks)))), '${INPUT}', '${INPUT}_'],
+        "output": ['${INPUT}_%s' % suffix for suffix in string.ascii_lowercase[:chunks]],
         "post_function": split
     },
 
@@ -54,7 +61,6 @@ gasw_repo = {
         "executable": "gshuf",
         "arguments": ['${INPUT}', '>', '${INPUT}.bam'],
         "output": ['${INPUT}.bam'],
-        # "output": ['fastq_0000.bam'],
         # "post_function": bwa
     },
 
