7898c0bce031ce7d357f1fbf9f604dcdab7d5c63
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2016-07-28 14:18:24 +0200

    Make portable.

--- examples/split.sh
+++ examples/split.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if test `uname` == "Darwin"; then
+CHUNKSIZE=$(expr `stat -f %z $1` / $2)
+else
 CHUNKSIZE=$(expr `stat -c %s $1` / $2)
+fi
+
 split -a 1 -b $CHUNKSIZE $1 $3
 sleep $4
